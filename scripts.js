4a682ad1bf4fd0aa1c4ac30141a679bd6b4edae5
vraj-maheshwari <vraj9081354565@gmail.com>
2025-02-27 23:30:11 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-// You can add custom JavaScript functions here if necessary
-// For example, initializing the carousel or other interactions
-// Example:
-document.addEventListener('DOMContentLoaded', function() {
-    // Example of initializing the carousel programmatically
-    const carousel = new bootstrap.Carousel('#project1Carousel');
-});
